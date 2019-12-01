c8b11b287a0c217e8eb583159976bb3e9ab6d68b
dizak <dariusz.izak@ibb.waw.pl>
2019-12-01 19:55:37 +0100

    Remove DetailView.template_name
    No more need for DetailView.template_name since template is named
    lab_detail.html now
    Please enter the commit message for your changes. Lines starting

--- django_team_websites/lab_website/views.py
+++ django_team_websites/lab_website/views.py
@@ -11,6 +11,5 @@ class IndexView(DetailView):
     """
     View for index page. Based on django.views.generic.detail.DetailView
     """
-    template_name = 'lab_website/index.jj2.html'
     model = models.Lab
     slug_field = 'name_url'
